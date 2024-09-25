98a7cdcdb6f1acdab67a1922d97f379a24379868
Muhannad <muhanned9980@gmail.com>
2024-09-25 16:57:13 +0300

    finished until exercise 5

---
+++ app.js
@@ -0,0 +1,14 @@
+//Exercise 1: Define an empty array
+const foods = [];  
+
+console.log('Exercise 1 result:', foods);
+//Add strings to the array
+foods.push('pizza', 'cheeseburger');
+console.log('Exercise 2 result:', foods);
+//Exercise 3: Insert at the beginning
+foods.unshift('taco');
+console.log('Exercise 3 result:', foods);
+//Exercise 4: Access an array element
+const favFood = foods[1];
+console.log('Exercise 4 result:', favFood);
+//Exercise 5: Insert an element between two others
